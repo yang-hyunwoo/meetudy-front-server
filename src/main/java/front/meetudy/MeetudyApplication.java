5c171e32716207687bcf660ea3b22dd9250fe68e
cohouseol <cohouseol@gmail.com>
2025-07-14 18:52:49 +0900

    refactor: 배포를 위한 환경설정 수정

--- src/main/java/front/meetudy/MeetudyApplication.java
+++ src/main/java/front/meetudy/MeetudyApplication.java
@@ -1,9 +1,12 @@
 package front.meetudy;
 
 import front.meetudy.property.FrontJwtProperty;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 //@EnableConfigurationProperties(FrontJwtProperty.class)
@@ -11,6 +14,12 @@ public class MeetudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetudyApplication.class, args);
+	}
+
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
